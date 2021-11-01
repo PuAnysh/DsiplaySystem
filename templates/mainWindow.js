7f7db89f43d4112b32b9655d96201d45c1f1d6da
Puanysh <814019262@qq.com>
2021-11-01 14:58:22 +0800

    update template images

--- templates/mainWindow.js
+++ templates/mainWindow.js
@@ -6,10 +6,14 @@ inputObj.setAttribute('style', 'visibility:hidden');
 inputObj.value
 document.body.appendChild(inputObj);
 
+
+var original_src, fpn1,fpn2,fpn3,fpn4,fpn5
+
 inputObj.addEventListener('change', e => {
     console.log(e.target.files)
     entry = e.target.files[0]
     document.querySelector('#display').src=entry.path
+    original_src = entry.path
     }
 );
 
@@ -22,6 +26,25 @@ document.querySelector("#closeBtn").addEventListener("click", ()=> {
 
 document.querySelector("#open_file").addEventListener("click", ()=> {
     inputObj.click()
+})
+document.querySelector("#fpn1").addEventListener("click", ()=> {
+    document.querySelector('#display').src="./img/mask.jpg"
+})
+document.querySelector("#fpn2").addEventListener("click", ()=> {
+    document.querySelector('#display').src="./img/mask.jpg"
+})
+document.querySelector("#fpn3").addEventListener("click", ()=> {
+    document.querySelector('#display').src="./img/mask.jpg"
+})
+document.querySelector("#fpn4").addEventListener("click", ()=> {
+    document.querySelector('#display').src="./img/mask.jpg"
     
 })
-
+document.querySelector("#fpn5").addEventListener("click", ()=> {
+    document.querySelector('#display').src="./img/mask.jpg"
+    
+})
+document.querySelector("#original").addEventListener("click", ()=> {
+    
+    document.querySelector('#display').src="./img/original.jpg"
+})
